9d1aec458b9689ed91b79ebb6ff524ac28d7f39f
Sanjeev Panicker/USA <Sanjeevkumar.Panicker@cushwake.com>
2020-02-17 17:44:09 -0500

    Updates

---
+++ precache-manifest.2fcb7e09ed256160678fa6747c68c12c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f32415001de79546a3eef65e17ebf3e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a72b11814c68f346279",
+    "url": "/monsters-rolodex/static/css/main.040714dd.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a72b11814c68f346279",
+    "url": "/monsters-rolodex/static/js/main.87f610d2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
